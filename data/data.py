0e514a1af57bc984889143109ddb2f0f86819e26
Anurag Paul <anurag1paul@gmail.com>
2019-04-13 20:45:00 -0700

    creating class PupilData

--- data/data.py
+++ data/data.py
@@ -9,3 +9,17 @@ class JointAttentionData:
 
     def __repr__(self):
         return str(self.__dict__)
+
+
+class PupilData:
+
+    def __init__(self, glass_id, timestamp, eye_id, norm_pos, confidence):
+        self.glass_id = glass_id
+        self.eye_id = eye_id
+        self.norm_pos = norm_pos
+        self.confidence = confidence
+        self.timestamp = timestamp
+
+    def __repr__(self):
+        return str(self.__dict__)
+
